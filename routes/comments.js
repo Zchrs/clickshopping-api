24ae5d946d636e65401bbfa2f483102ad95567b6
Zchrs <gustavo.a.c.m@gmail.com>
2026-02-16 10:52:39 -0500

    frontend  only

--- routes/comments.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createComment, getComents } = require("../controllers/comments");
-
-router.post("/post", createComment);
-router.get("/get", getComents);
-
-module.exports = router;
--- routes/invitations.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateInvitationCode, verifyInvitationCode } = require('../controllers/invitations');
-
-router.post('/generate', generateInvitationCode);
-router.get('/verify/:code', verifyInvitationCode);
-
-module.exports = router;
